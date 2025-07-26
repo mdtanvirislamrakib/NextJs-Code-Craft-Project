9acd619fce145961d6303d40f4c83a177c775225
mdtanvirislamrakib <mdtanvirislamrakib7@gmail.com>
2025-07-26 23:50:12 +0600

    add language

--- src/app/(root)/_components/ThemeSelector.tsx
+++ src/app/(root)/_components/ThemeSelector.tsx
@@ -5,7 +5,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { THEMES } from "../_constants";
 import { AnimatePresence, motion } from "framer-motion";
 import { CircleOff, Cloud, Github, Laptop, Moon, Palette, Sun } from "lucide-react";
-// import useMounted from "@/hooks/useMounted";
+import useMounted from "@/hooks/useMounted";
 
 const THEME_ICONS: Record<string, React.ReactNode> = {
   "vs-dark": <Moon className="size-4" />,
@@ -17,8 +17,7 @@ const THEME_ICONS: Record<string, React.ReactNode> = {
 
 function ThemeSelector() {
   const [isOpen, setIsOpen] = useState(false);
-  // const mounted = useMounted();
-  const [mounted, setMounted] = useState(false)
+  const mounted = useMounted();
   const { theme, setTheme } = useCodeEditorStore();
   const dropdownRef = useRef<HTMLDivElement>(null);
   const currentTheme = THEMES.find((t) => t.id === theme);
@@ -34,9 +33,6 @@ function ThemeSelector() {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
-  useEffect(() => {
-    setMounted(true)
-  }, [isOpen])
 
   if (!mounted) return null;
 
