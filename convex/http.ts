ec1ea14b6651f07c7456691157d6243771ee9be9
mdtanvirislamrakib <mdtanvirislamrakib7@gmail.com>
2025-07-26 10:33:46 +0600

    create http

--- convex/http.ts
+++ convex/http.ts
@@ -1,5 +1,7 @@
 import { httpRouter } from "convex/server";
 import {httpAction} from "./_generated/server"
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { Webhook } from "svix";
 
 const http = httpRouter()
 
@@ -18,5 +20,50 @@ http.route({
     const svix_id = request.headers.get("svix-id");
     const svix_signature = request.headers.get("svix-signature");
     const svix_timestamp = request.headers.get("svix-timestamp");
+
+    if (!svix_id || !svix_signature || !svix_timestamp) {
+      return new Response("Error occurred -- no svix headers", {
+        status: 400,
+      });
+    }
+
+    const payload = await request.json();
+    const body = JSON.stringify(payload);
+
+    const wh = new Webhook(webhookSecret);
+    let evt: WebhookEvent;
+
+    try {
+      evt = wh.verify(body, {
+        "svix-id": svix_id,
+        "svix-timestamp": svix_timestamp,
+        "svix-signature": svix_signature,
+      }) as WebhookEvent;
+    } catch (err) {
+      console.error("Error verifying webhook:", err);
+      return new Response("Error occurred", { status: 400 });
+    }
+
+    const eventType = evt.type;
+    if(eventType === "user.created") {
+      const { id, email_addresses, first_name, last_name } = evt.data;
+      const email = email_addresses[0].email_address;
+      const name = `${first_name || ""} ${last_name || ""}`.trim();
+
+      try {
+        await ctx.runMutation(api.users.syncUser, {
+          userId: id,
+          email,
+          name,
+        });
+      } catch (error) {
+        console.log("Error creating user:", error);
+        return new Response("Error creating user", { status: 500 });
+      }
+    }
+
+    return new Response("Webhook processed successfully", { status: 200 });
   })
 })
+
+export default http;
